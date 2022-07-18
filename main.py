86ff5bd2a84934aa0565bbc841e45d06dfa02953
Redwane <redwanehammas@gmail.com>
2022-07-18 21:04:55 +0200

    Add minimal code

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import pygame as pg
+
 def main():
-    pass
+    pg.init()
+
+    # logo = pg.image.load('logo.png')
+    # pg.display.set_icon(logo)
+    pg.display.set_caption('Tetris')
+
+    pg.display.set_mode((800, 600))
+
+    while 1:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return None
 
 
 if __name__ == '__main__':
