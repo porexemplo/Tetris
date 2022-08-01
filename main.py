176af7abcd86d0320fdc4e223c7eeacd05529b65
Redwane <redwanehammas@gmail.com>
2022-08-01 14:46:43 +0200

    Add border drawing

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 import pygame as pg
 import os
 from structs import Cell, Grid
-from settings import *
+from game_data import *
 
 
-grid = Grid()
+grid = Grid()   
 
 
-def draw_bounds():
-    for i in range(H_BOUND):
-        grid.get_cell(0, i).set_content(bg_square)
-        grid.get_cell(W_BOUND, i).set_content(bg_square)
-
-    for i in range(W_BOUND):
-        grid.get_cell(i, H_BOUND).set_content(bg_square)        
-
-
-def init_window():
+def update_window():
     logo = pg.image.load('logo.png')
     pg.display.set_icon(logo)
     pg.display.set_caption('Tetris by YummyBread')
     
-    SCREEN.fill(BLACK)
-    # pg.display.update()
+    SCREEN.fill(RGB_BLACK)
+    pg.display.update()
 
 
 def main():
     pg.init()
-    init_window()
-    grid.render()
+    update_window()
+    grid.set_border()
+    grid.draw(SCREEN)
+    pg.display.update()
 
     while 1:    # Main game loop
         for event in pg.event.get():
